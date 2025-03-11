bafb0bdd10fc024c9e1ea6e4500c0aa86585af84
Toan Doan <toandev.95@gmail.com>
2025-03-11 09:25:48 +0700

    refactor: remove Prisma types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,1 @@
-import {
-  Conversation as PrismaConversation,
-  Document as PrismaDocument,
-  Message as PrismaMessage,
-} from "@prisma/client";
-
-export type Conversation = PrismaConversation & {
-  messages: Message[];
-};
-
-export type Message = PrismaMessage & {
-  conversation: Conversation;
-};
-
-export type Document = PrismaDocument & {};
+// TODO: Define types for the project
