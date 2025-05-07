a71b8fe42f73d30f20ab17d3e4410d27a77d90e4
PhucNguyen <catpx4689@gmail.com>
2025-05-07 19:50:57 +0700

    fix: correct Minio endpoint

--- src/utils/minio.ts
+++ src/utils/minio.ts
@@ -1,7 +1,7 @@
 import * as Minio from 'minio';
 
 export const minioClient = new Minio.Client({
-  endPoint: 'localhost',
+  endPoint: 'minio',
   port: 9000,
   useSSL: false,
   accessKey: 'minioadmin',
